7f03d196d539da3e6fb2a2e347fc903914438b0a
vrossi96 <vrossi396@yahoo.it>
2022-03-01 13:40:31 +0100

    imported data in php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include '../api/data.php';
+
+var_dump($database[0]['title']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP AJAX DISCHI M1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
