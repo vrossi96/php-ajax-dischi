d2e312319255eaa635699d47278bbde3100a5eba
vrossi96 <vrossi396@yahoo.it>
2022-03-01 14:27:09 +0100

    created card

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 include '../api/data.php';
-
-var_dump($database[0]['title']);
 ?>
 
 <!DOCTYPE html>
@@ -11,11 +9,29 @@ var_dump($database[0]['title']);
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="./css/style.css">
+   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous' />
    <title>PHP AJAX DISCHI M1</title>
 </head>
 
 <body>
-
+   <header></header>
+   <main class="d-flex align-items-center">
+      <div class="card-container container">
+         <div class="row">
+            <div class="col-1-5">
+               <div class="card-song text-center">
+                  <img class="img-fluid" src="<?php echo $database[0]['poster'] ?>" alt="">
+                  <p class="song-title"><?php echo $database[0]['title'] ?></p>
+                  <div>
+                     <p class="song-desc"><?php echo $database[0]['author'] ?></p>
+                     <p class="song-desc"><?php echo $database[0]['year'] ?></p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </main>
 </body>
 
 </html>
